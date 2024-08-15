41de476cc71ecda721ad7468ab7d6b2ac36b9225
SONG <thdtjdgml415@naver.com>
2024-08-15 22:24:47 +0900

    [fix] 회원가입 코드 수정 - AuthService 사용 안함

--- features/auth/hooks/useLogin.ts
+++ features/auth/hooks/useLogin.ts
@@ -1,37 +1,37 @@
-import AuthService from "@/features/auth/api/AuthService";
-import { SuccessLoginData, User } from "@/features/auth/model/auth";
-import useAlert from "@/hooks/useAlert";
-import Service from "@/share/api/Service";
-import { useMutation } from "@tanstack/react-query";
-import { AxiosError } from "axios";
+// import AuthService from "@/features/auth/api/AuthService";
+// import { SuccessLoginData, User } from "@/features/auth/model/auth";
+// import useAlert from "@/hooks/useAlert";
+// import Service from "@/share/api/Service";
+// import { useMutation } from "@tanstack/react-query";
+// import { AxiosError } from "axios";
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
-export const useLogin = () => {
-  const router = useRouter();
-  const service = new Service();
-  const openAlert = useAlert((state) => state.openAlert);
-  const mutation = useMutation({
-    mutationFn: (data: User) => AuthService.login(data),
-    onSuccess: ({ data }: SuccessLoginData) => {
-      console.log("suceess login data -", data);
-      if (data) {
-        service.setAuthToken(data.tokenInfo.accessToken);
-        service.setAuthRefreshToken(data.tokenInfo.refreshToken);
-        localStorage.setItem("role", data.role);
-        router.push("/chat/chatroom");
-      }
-    },
-    onError: (error: AxiosError<any, any>) => {
-      if (error.isAxiosError && error.response) {
-        openAlert({
-          type: "error",
-          title: "로그인 에러",
-          message: error?.response?.data.message,
-        });
-      }
-    },
-  });
+// export const useLogin = () => {
+//   const router = useRouter();
+//   const service = new Service();
+//   const openAlert = useAlert((state) => state.openAlert);
+//   const mutation = useMutation({
+//     mutationFn: (data: User) => AuthService.login(data),
+//     onSuccess: ({ data }: SuccessLoginData) => {
+//       console.log("suceess login data -", data);
+//       if (data) {
+//         service.setAuthToken(data.tokenInfo.accessToken);
+//         service.setAuthRefreshToken(data.tokenInfo.refreshToken);
+//         localStorage.setItem("role", data.role);
+//         router.push("/chat/chatroom");
+//       }
+//     },
+//     onError: (error: AxiosError<any, any>) => {
+//       if (error.isAxiosError && error.response) {
+//         openAlert({
+//           type: "error",
+//           title: "로그인 에러",
+//           message: error?.response?.data.message,
+//         });
+//       }
+//     },
+//   });
 
-  return mutation;
-};
+//   return mutation;
+// };
--- features/auth/hooks/useSignup.ts
+++ features/auth/hooks/useSignup.ts
@@ -1,14 +1,14 @@
-import AuthService from "@/features/auth/api/AuthService";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import useAlert from "../../../hooks/useAlert";
+import { postSignUp } from "../api/AuthService";
 
 export const useSignUp = <T>() => {
   const router = useRouter();
   const openAlert = useAlert((state) => state.openAlert); // 알림 창을 열기 위한 상태
   const mutation = useMutation({
-    mutationFn: (data: T) => AuthService.postSignUp<T>(data),
+    mutationFn: (data: T) => postSignUp<T>(data),
     onSuccess: (data: T) => {
       console.log("success sign up data -", data);
       router.push("/sign-in");
