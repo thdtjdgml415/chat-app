224092309bc9ad2c1a968c21687109a00a873148
SONG <thdtjdgml415@naver.com>
2024-08-15 23:36:12 +0900

    [fix] rewrite설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,6 @@
 const analyzing = process.env.ANALYZE === "true";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        // http://43.203.222.95:8080/api/chat/rooms
-        source: "/api/chat/rooms", // 요청한 경로
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/chat/rooms`, // 내가 사용할 경로
-      },
-      {
-        // http://43.203.222.95:8080/api/member/colleague'
-        source: "/api/member/colleague", // 요청한 경로
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/member/colleague`, // 내가 사용할 경로
-      },
-    ];
-  },
   experimental: {
     staleTimes: {
       dynamic: 0,
