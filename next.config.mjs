9c9ce27d9483b21e3b70aedda532c70b23428df2
SONG <thdtjdgml415@naver.com>
2024-08-15 23:17:25 +0900

    [fix] 제발 수정좀..

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         // http://43.203.222.95:8080/api/member/colleague'
-        source: "/api/member/colleague", // 요청한 경로
+        source: "https://43.203.222.95:8080/api/member/colleague", // 요청한 경로
         destination: `http://43.203.222.95:8080/api/member/colleague`, // 내가 사용할 경로
       },
     ];
